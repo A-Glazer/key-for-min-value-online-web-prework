742b6c174b06b7d1b8f8d007c702e558488c531f
A Glazer <avigayilglazer@gmail.com>
2019-01-09 13:15:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,9 @@ name_hash={
 
 def key_for_min_value(name_hash)
   name_hash.each do |kids_name, age|
-    num = "#{age}"
+    num = age
   end
-  name_hash.collect do |a, b|
+  num.collect do |a, b|
     if a == b
       0
     elsif a < b
