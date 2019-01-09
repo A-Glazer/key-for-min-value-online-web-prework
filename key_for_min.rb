08a9c475bb3490a2c2b4762fc71344a75babc12d
A Glazer <avigayilglazer@gmail.com>
2019-01-09 13:38:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,23 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 name_hash={
-  "Timmy" => 9,
-  "Sarah" => 6,
-  "Amanda" => 27
+  :Timmy => 9,
+  :Sarah => 6,
+  :Amanda => 27
 }
 
 def key_for_min_value(name_hash)
   name_hash.collect do |kids_name, age|
     age
   end
-  name_hash.collect do |a, b|
-    if a == b
-      0
-    elsif a < b
-      -1
-    elsif a > b
-      1
-    end
+
 
 
   end
